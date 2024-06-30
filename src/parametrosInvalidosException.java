0a1d10c86d4a916d5a7f621dd9e1390d527bdca9
jadsonojr <jadson.personal.edf@gmail.com>
2024-06-30 00:57:44 -0300

    agora foi

--- src/parametrosInvalidosException.java
+++ src/parametrosInvalidosException.java
@@ -1,6 +1,3 @@
-public class parametrosInvalidosException extends Exception {
+public class ParametrosInvalidosException extends Exception {
 
-    public parametrosInvalidosException(String message) {
-        super(message);
-    }
 }
